fa07bce88108b01a78ad0d1457ff801cbdf032a0
rakesh-kale <Rakeshkale336@gmail.com>
2026-03-16 18:02:32 +0530

    Initial School Management MVC project

---
+++ Data/SchoolManagementDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace SchoolManagementApp.MVC.Data
+{
+    public class SchoolManagementDbContextFactory
+        : IDesignTimeDbContextFactory<SchoolManagementDbContext>
+    {
+        public SchoolManagementDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<SchoolManagementDbContext>();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new SchoolManagementDbContext(optionsBuilder.Options);
+        }
+    }
+}
